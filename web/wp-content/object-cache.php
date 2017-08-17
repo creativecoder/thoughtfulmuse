4f1377cb789437320720fe808d49fed480af1331
Grant Kinney <grant@exygy.com>
2017-08-17 17:38:32 -0500

    Enables redis object caching

--- web/wp-content/object-cache.php
+++ web/wp-content/object-cache.php
@@ -3,5 +3,5 @@
  * Disable object caching for local development
  */
 if ( ! defined('WP_ENV') || 'local' !== WP_ENV ) {
-	// require_once( WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php' );
+	require_once( WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php' );
 }
