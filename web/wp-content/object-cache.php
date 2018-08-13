c0bba05edd752bee415010b99bcb1cc9e9282ec0
Grant Kinney <grant.kinney@automattic.com>
2018-08-12 22:59:35 -0500

    Updates object-cache.php to symlink to plugin

--- web/wp-content/object-cache.php
+++ web/wp-content/object-cache.php
@@ -1,7 +1,1 @@
-<?php
-/**
- * Disable object caching for local development
- */
-if ( ! defined('WP_ENV') || 'local' !== WP_ENV ) {
-	require_once( WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php' );
-}
+plugins/wp-redis/object-cache.php
